85d0379eafb0c16df5a75ca83f3ada3bb53ade98
Kevin Siegler <kevinsiegler54@gmail.com>
2020-06-24 14:55:59 -0500

    implement initiative editor frontend
    This commit implements virtually the same frontend that's been demoed.
    
    A couple styling considerations should be noted:
    1. This frontend utilizes the out of the box materialUI dark theme
    2. the `id` prop for Initiative entries and the `key` prop for
    contributions are rendered as hidden. So for as I can tell right now,
    	      this is necessary in order for the uuid value to be
    	      generated for the key prop, but not for the id prop. This
    	      is because initial values are not so easily populated for
    	      the entire contribution because it's iterable. Some custom
    	      work might be needed to implement this more elegantly and
    	      prevent tampering with the DOM
    
    The initiative shape will live in the App.js file until it is further
    discussed as a part of Milestone 2. Then it will be moved to its own
    file.
    
    Test plan: Testing is manual. A mock state is provided and rendered.
    All CRUD operations should be possible, and the state can be exported
    as a json file, and saved to the file system.

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,43 +1,135 @@
 // @flow
 
 import React from "react";
-import {Admin, Resource, ListGuesser, EditGuesser} from "react-admin";
+import {Admin, Resource, ListGuesser, EditGuesser, Loading} from "react-admin";
+import {
+  InitiativeList,
+  InitiativeCreate,
+  InitiativeEdit,
+} from "./components/Initiatives";
 import fakeDataProvider from "ra-data-fakerest";
 import {createMemoryHistory} from "history";
+import {createMuiTheme} from "@material-ui/core/styles";
+import {Graph, type Node} from "../core/graph";
+import {fakeInitiatives} from "./mock/fakeInitiatives";
 
-async function loadAndReport(path) {
+async function loadAndReport(path: string) {
   const response = await fetch(path);
   if (!response.ok) {
     console.error(path, response);
   }
-  const json = await response.json();
-  console.log(path, json);
+  return response.json();
 }
 
-const dataProvider = fakeDataProvider({
-  people: [
-    {id: 0, title: "Hello, world!"},
-    {id: 1, title: "FooBar"},
-    {id: 2, title: "Bar foo"},
-  ],
-  comments: [
-    {id: 0, person_id: 0, author: "John Doe", body: "Sensational!"},
-    {id: 1, person_id: 0, author: "Jane Doe", body: "I agree"},
-  ],
-});
+function loadUsers(plugins, graph): Array<Object> {
+  const userPrefixes = plugins.reduce((acc, {userTypes}) => {
+    userTypes.forEach((type) => acc.push(type.prefix));
+    return acc;
+  }, []);
+  const users = [];
+  userPrefixes.forEach((prefix) => {
+    const userIterator = graph.nodes({prefix});
+    let nextUser = userIterator.next();
+    while (!nextUser.done) {
+      const user = {
+        ...nextUser.value,
+      };
+      users.push(user); // find a way to return users, possibly refactor
+      nextUser = userIterator.next();
+    }
+  });
+  return users;
+}
+
+function loadActivities(plugins, graph): Array<Object> {
+  const activityPrefixes = plugins.reduce((acc, {nodeTypes}) => {
+    nodeTypes.forEach(
+      (type) =>
+        !["Bot", "User", "Identity", "Like"].find((n) => n === type.name) &&
+        acc.push(type.prefix)
+    );
+    return acc;
+  }, []);
+  const activities = [];
+  activityPrefixes.forEach((prefix) => {
+    const activityIterator = graph.nodes({prefix});
+    let nextActivity = activityIterator.next();
+    while (!nextActivity.done) {
+      const activity = {
+        ...nextActivity.value,
+      };
+      activities.push(activity); // find a way to return activities here, possibly refactor
+      nextActivity = activityIterator.next();
+    }
+  });
+  return activities;
+}
+
+const dataProvider = fakeDataProvider(fakeInitiatives, true);
 
 const history = createMemoryHistory();
 
-export default class App extends React.Component<{||}> {
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+export type AppState = {|
+  graph: ?Graph,
+  plugins: ?Object,
+  users: Node[],
+  activities: Node[],
+  loaded: boolean,
+|};
+
+export default class App extends React.Component<{||}, AppState> {
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      graph: null,
+      plugins: null,
+      users: [],
+      activities: [],
+      loaded: false,
+    };
+  }
   async componentDidMount() {
-    loadAndReport("sourcecred.json");
-    loadAndReport("output/credResult.json");
-    loadAndReport("config/sourcecred/discourse/config.json");
+    const [
+      ,
+      {
+        weightedGraph: [, {graphJSON}],
+        plugins: [, plugins],
+      },
+    ] = await loadAndReport("output/credResult.json");
+    const graph = Graph.fromJSON(graphJSON);
+    const users = loadUsers(plugins, graph);
+    const activities = loadActivities(plugins, graph);
+    this.setState({
+      graph,
+      plugins,
+      users,
+      activities,
+      loaded: true,
+    });
   }
 
   render() {
+    if (!this.state.loaded)
+      return (
+        <Loading
+          loadingPrimary="Fetching cred details..."
+          loadingSecondary="Your patience is appreciated"
+        />
+      );
     return (
-      <Admin dataProvider={dataProvider} history={history}>
+      <Admin theme={theme} dataProvider={dataProvider} history={history}>
+        <Resource
+          name="initiatives"
+          list={InitiativeList}
+          create={InitiativeCreate(this.state)}
+          edit={InitiativeEdit(this.state)}
+        />
         <Resource name="people" list={ListGuesser} edit={EditGuesser} />
         <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
       </Admin>
